208611a8ec7dc5e926203610323b7391f2366343
songminseok <ske3056@gmail.com>
2024-01-10 15:58:16 +0900

    feat: 현재 활성화시킬 씬 결정하기

--- _study/js/main.js
+++ _study/js/main.js
@@ -1,4 +1,8 @@
 (() => {
+  let yOffset = 0; // window.scrollY 대신 쓸 변수
+  let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0; // 현재 활성화된(눈 앞에 보고 있는) 씬(scroll section)
+
   const sceneInfo = [
     {
       type: "sticky",
@@ -35,14 +39,37 @@
   ];
 
   function setLayout() {
+    console.log("???");
     for (let i = 0; i < sceneInfo.length; i++) {
       sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight;
       sceneInfo[
         i
       ].obj.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
-    console.log(sceneInfo);
   }
+
+  function scrollLoop() {
+    prevScrollHeight = 0;
+
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight;
+    }
+    console.log("prevScrollHeight : ", prevScrollHeight);
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++;
+    }
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return;
+      currentScene--;
+    }
+    console.log(currentScene);
+  }
+
   window.addEventListener("resize", setLayout);
+  window.addEventListener("scroll", () => {
+    yOffset = window.scrollY;
+    scrollLoop();
+  });
   setLayout();
 })();
